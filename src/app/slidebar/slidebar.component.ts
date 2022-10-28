8fd459bb44b63a934cc22817deb22bf2d678dcf7
Emiliano Daian <emilianodaian@gmail.com>
2022-10-28 16:12:31 -0300

    nuevo

--- src/app/slidebar/slidebar.component.ts
+++ src/app/slidebar/slidebar.component.ts
@@ -12,4 +12,6 @@ export class SlidebarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public imagen1:string ="https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1280x720.png";
+
 }
